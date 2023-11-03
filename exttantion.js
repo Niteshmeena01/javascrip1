077a6074d91c94fc8c37c72aba621e67bc46a6a6
Niteshmeena01 <149501086+Niteshmeena01@users.noreply.github.com>
2023-11-03 18:55:37 +0530

    new changes
    ok

---
+++ exttantion.js
@@ -0,0 +1,215 @@
+
+// const Nitesh=123
+
+// let Devansahu=55
+//  var x= null  
+
+// var Vikas=222 
+// var Vikas=333   
+// var     z= Nitesh%Devansahu
+      
+// console.log( Nitesh,Devansahu,Vikas,);
+// console.log(z);
+// console.log(typeof z)
+
+// If&Else Odd Eeven
+//    var even=
+// if (even==2) {console.log("even number"); }
+// else {console.log("odd number");}
+// if (even==4) {console.log("even number"); }
+// else {console.log("odd number");}
+// if (even==6) {console.log("even number"); }
+// else {console.log("odd number");}
+// if (even==8) {console.log("even number"); }
+// else {console.log("odd number");}
+// if (even==10) {console.log("even number"); }
+// else {console.log("odd number");}
+
+
+// Ifelse Statement
+// var per=90
+
+// if (per>=33 && per<=45 ){console.log("Pass");}
+// else if(per>=46 && per<=60){console.log('3rd Divison');}
+// else if(per>=61 && per<=75){console.log('2nd Divison');}
+// else if(per>=76 && per<=85){console.log('1st Divison');}
+// else if(per>=86 && per<=100){console.log('Merit');}
+
+// var number=26
+
+// if (number%2==0){ console.log('this is even number');}
+// else{console.log('odd number');}
+
+
+
+
+// var a=    
+// var b=
+//  if (a==10){console.log( 'a=20');}
+//   if(b==20) {console.log('b=10');}
+//  else {console.log('this is not a valid number');}
+
+// swicth concept
+
+
+
+// var day=3
+
+//  switch (day) {
+//           case 0: console.log('Sunday');
+          
+//           break;
+//           case 1: console.log('Monday');
+          
+//           break;
+//           case 2: console.log('Tuesday');
+          
+//           break;
+//           case 3:console.log('Wednesday');
+          
+//           break;
+//           case 4:console.log('Thursday');
+          
+//           break;
+//           case 5:console.log('Friday');
+          
+//           break;
+//           case 6:console.log('Saturday');
+          
+//           break;
+ 
+//      default:console.log('This not a valid day');
+//           break;
+//  }
+
+// Even Number
+// var i=2
+// while (i<=50) { 
+//      console.log(i);
+//      i=i+2
+//      }
+
+// odd number
+     // var i=1
+     // while (i<=50) { 
+     //      console.log(i);
+     //      i=i+2
+     //      }
+
+
+// 10 table
+
+// var i=10
+// while(i<=100)
+// {console.log(i);
+//  i=i+10}
+
+// 12 table
+ 
+// var i=12
+// while (i<=120){console.log(i); i=i+12}
+
+// 20 table
+// var i=20
+// while(i<=200){console.log(i); i=i+20}
+
+// factors
+// i=1
+// while (i=1){console.log(i);
+//               i=i*1}
+
+// var a =200;
+// var b;
+ 
+// ( a == 400)? b ="True" : b ="False"
+// console.log(b);
+
+
+// let x=20
+//     x=30
+//     console.log(x);
+// "use strict"
+// var x=20
+// var y='40'
+
+//     console.log('y'-'x');
+    
+//      document.getElementById(h1)
+     
+
+
+// var a=10 
+// console.log(++a);
+// console.log(a++);
+// console.log(a);
+// console.log(--a);
+// console.log(a--);
+// console.log(a);
+// console.log(--a);
+
+// increment & Dicreament
+
+// var a=7
+
+// console.log(a--);
+// console.log(a++);
+// console.log(a++);
+// console.log(a);
+// console.log(a--);
+// console.log(a--);
+// console.log(a++);
+// console.log(++a);
+// console.log(a);
+
+// var a=10
+// var b=12
+// console.log(a<b);
+// console.log(a>b);
+
+// a=15
+// b=16
+// c=17
+// D=15
+// console.log(a==b||b===c||c===a||a==D);
+// console.log(a==b&&b===c&&c===a&&a==D);
+
+// a=10
+// b=15
+// c=a
+// a=b
+// b=c
+
+// console.log(a,b);
+
+
+// var a=200;
+// var b;
+
+// a==200? console.log(true) : console.log(false); 
+
+
+// var i=1
+//  while (i<=5) {console.log("vijay"); i++}
+
+// var a="bhaskar"
+// var b="bhaskar"
+
+// console.log(a+b);
+
+// Concatination&template literal
+// var a="Bhaskar"
+// var b="yogi"
+// var my_name=a+b
+// console.log("I am "+my_name);
+// console.log(`I am ${my_name}`);
+
+
+
+// a="jay"
+// b="Vijay"
+
+// console.log(`${a} or ${b}`)
+//  var a=undefined;
+//  console.log(typeof(a));
+
+ 
